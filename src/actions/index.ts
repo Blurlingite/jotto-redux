5ef7fb23bd2fe5459197c60e505fa07613772765
Vishnu Dawah <vkdawah@gmail.com>
2020-04-18 12:41:14 -0400

    Set up guessWords tests

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,7 +1,15 @@
 export const actionTypes = {
   CORRECT_GUESS: "CORRECT_GUESS",
+  GUESS_WORD: "GUESS_WORD",
 };
 
-export function correctGuess() {
-  return { type: actionTypes.CORRECT_GUESS };
-}
+/**
+ * @function guessWord
+ * @param {string} guessedWord - Guessed word.
+ * @returns {function} - Redux Thunk function.
+ */
+// TODO: find out how to type this action creator (Redux Thunk)
+export const guessWord = (guessedWord: string) => {
+  // this is possible b/c of Redux Thunk middleware
+  return function (dispatch: any, getState: any) {};
+};
