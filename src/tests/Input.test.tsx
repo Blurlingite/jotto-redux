3e6822b41e684d9a731116526c425522a49c0678
Vishnu Dawah <vkdawah@gmail.com>
2020-04-17 20:41:52 -0400

    Complete Input component render tests for success=false

--- src/tests/Input.test.tsx
+++ src/tests/Input.test.tsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { shallow, ShallowWrapper } from "enzyme";
 import { findByTestAttr, storeFactory } from "../test/testUtils";
 
 import Input from "../components/Input";
+import { ShallowRenderer } from "react-dom/test-utils";
 
 const setup = (initialState = {}) => {
   // use whatever you passed in to populate the state in the store
@@ -21,9 +22,23 @@ const setup = (initialState = {}) => {
 
 describe("render", () => {
   describe("word has not been guessed", () => {
-    test("render component w/o error", () => {});
-    test("renders input box", () => {});
-    test("renders submit button", () => {});
+    let wrapper: ShallowWrapper;
+    beforeEach(() => {
+      const initialState = { success: false };
+      wrapper = setup(initialState);
+    });
+    test("render component w/o error", () => {
+      const component = findByTestAttr(wrapper, "component-input");
+      expect(component.length).toBe(1);
+    });
+    test("renders input box", () => {
+      const inputBox = findByTestAttr(wrapper, "input-box");
+      expect(inputBox.length).toBe(1);
+    });
+    test("renders submit button", () => {
+      const submitButton = findByTestAttr(wrapper, "submit-button");
+      expect(submitButton.length).toBe(1);
+    });
   });
 
   describe("word has been guessed", () => {
