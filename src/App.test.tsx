1718243fd17ef6c4bd10ef368d3dbd95b1ff7e39
Vishnu Dawah <vkdawah@gmail.com>
2020-04-19 19:42:02 -0400

    Connect App component (and tests)

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,43 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { shallow } from "enzyme";
 import App from "./App";
 
-test("renders learn react link", () => {
-  // const { getByText } = render(<App />);
-  // const linkElement = getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
+import { storeFactory } from "../src/test/testUtils";
+
+const setup = (state = {}) => {
+  const store = storeFactory(state);
+  const wrapper = shallow(<App store={store} />)
+    .dive()
+    .dive();
+
+  return wrapper;
+};
+
+describe("redux properties", () => {
+  test("success piece of state", () => {
+    const success = false;
+    const wrapper = setup({ success }) as any;
+    const successProp = wrapper.instance().props.success;
+    expect(successProp).toBe(success);
+  });
+
+  test("secretWord piece of state", () => {
+    const secretWord = "party";
+    const wrapper = setup({ secretWord }) as any;
+    const secretWordProp = wrapper.instance().props.secretWord;
+    expect(secretWordProp).toBe(secretWord);
+  });
+
+  test("guessWords piece of state", () => {
+    const guessedWords = [{ guessedWord: "train", letterMatchCount: 3 }];
+    const wrapper = setup({ guessedWords }) as any;
+    const guessedWordsProp = wrapper.instance().props.guessedWords;
+    expect(guessedWordsProp).toBe(guessedWords);
+  });
+
+  test("getSecretWord action creator as a function prop", () => {
+    const wrapper = setup() as any;
+    const getSecretWordProp = wrapper.instance().props.getSecretWord;
+    expect(getSecretWordProp).toBeInstanceOf(Function);
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,34 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+
 import "./App.css";
 import GuessedWords from "./components/GuessedWords";
 import Congrats from "./components/Congrats";
+import { getSecretWord } from "../src/actions";
+import Input from "./components/Input";
 
-class App extends Component {
+interface ComponentProps {
+  success: boolean;
+  secretWord: string;
+  guessedWords: [];
+}
+class App extends Component<ComponentProps> {
   render() {
     return (
       <div className="container">
         <h1>Jotto</h1>
-        <Congrats success={true} />
-        <GuessedWords
-          guessedWords={[{ guessedWord: "train", letterMatchCount: 3 }]}
-        />
+        <Congrats success={this.props.success} />
+        <Input />
+        <GuessedWords guessedWords={this.props.guessedWords} />
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state: any) => {
+  const { success, guessedWords, secretWord } = state;
+
+  return { success, guessedWords, secretWord };
+};
+
+export default connect(mapStateToProps, { getSecretWord })(App);
